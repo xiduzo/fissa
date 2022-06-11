e5fc49016a4b7bf6303dcf9dca34dcc6c45261db
Sander <mail@sanderboer.nl>
2022-06-11 23:29:08 +0200

    added spotify auth

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React, {useMemo} from 'react';
-import {AuthConfiguration, authorize} from 'react-native-app-auth';
+import {
+  AuthConfiguration,
+  authorize,
+  refresh,
+  AuthorizeResult,
+} from 'react-native-app-auth';
 import ToastContainer from './components/atoms/Toast';
 import Routes from './routes/Routes';
 import AddContextProvider from './routes/session/Room.AddContext';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 const App = () => {
   useMemo(async () => {
     const config: AuthConfiguration = {
+      usePKCE: false,
       clientId: 'a2a88c4618324942859ce3e1f888b938', // available on the app page
       redirectUrl: 'com.fissa:/oauth', // the redirect you defined after creating the app
       scopes: [
@@ -23,17 +30,34 @@ const App = () => {
       ], // the scopes you need to access
       serviceConfiguration: {
         authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-        tokenEndpoint: 'https://fissa--token-api.herokuapp.com/api/token',
+        tokenEndpoint:
+          'https://fissa-spotify-token-swap.herokuapp.com/api/token',
       },
     };
 
     try {
-      console.log(config);
-      const result = await authorize(config);
+      const item = await EncryptedStorage.getItem('accessToken');
+      const {accessTokenExpirationDate, refreshToken} = JSON.parse(
+        item ?? '',
+      ) as AuthorizeResult;
 
-      console.log(result);
+      // TODO: set timeout to update token
+      if (refreshToken) {
+        console.log('refreshToken', refreshToken);
+      }
+      if (new Date() < new Date(accessTokenExpirationDate)) {
+        // Token is still valid
+        console.log('token still valid');
+        return;
+      }
+
+      console.log('fetching token');
+      const result = await authorize(config);
+      console.log('got token', result);
+
+      await EncryptedStorage.setItem('accessToken', JSON.stringify(result));
     } catch (e) {
-      console.warn(e);
+      console.error(e);
     }
   }, []);
 
