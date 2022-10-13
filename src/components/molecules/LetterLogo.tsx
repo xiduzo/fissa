3da36f499d5208ad41f1e5fd492f8147b039b4df
Sander <mail@sanderboer.nl>
2022-10-13 02:19:19 +0200

    cleanup some code

--- src/components/molecules/LetterLogo.tsx
+++ src/components/molecules/LetterLogo.tsx
@@ -1,41 +1,33 @@
 import React, {FC, memo, useRef} from 'react';
 import {Animated, Dimensions, View} from 'react-native';
-import {Color, themes} from '../../types/Theme';
+import {themes} from '../../types/Theme';
 import LetterA from '../atoms/icons/LetterA';
 import LetterF from '../atoms/icons/LetterF';
 import LetterI from '../atoms/icons/LetterI';
 import LetterS from '../atoms/icons/LetterS';
-import {IconProps} from '../atoms/icons/_Icon';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-
+const letters = [LetterA, LetterF, LetterI, LetterS];
 const MovableLetter: FC = () => {
-  const prevPos = useRef({
-    x: Math.random() * windowWidth,
-    y: Math.random() * windowHeight,
-  });
-  const rotate = useRef(Math.random() * 360);
-
   const Component = useRef(
-    [LetterA, LetterF, LetterI, LetterS][Math.floor(Math.random() * 4)],
+    letters[Math.floor(Math.random() * letters.length)],
   ).current;
 
-  console.log('render');
   return (
     <Animated.View
       style={{
         width: '100%',
-        left: prevPos.current.x,
-        top: prevPos.current.y,
-        transform: [{rotate: rotate.current + 'deg'}],
+        left: Math.random() * windowWidth,
+        top: Math.random() * windowHeight,
+        transform: [{rotate: Math.random() * 360 + 'deg'}],
       }}>
       <Component
         style={{
           position: 'absolute',
 
           tintColor:
-            themes[Math.floor(Math.random() * themes.length)].main + '08',
+            themes[Math.floor(Math.random() * themes.length)].main + '05',
           // tintColor: '#00000020',
           transform: [{scale: 0.5}],
         }}
